0c601f0c53bd5e7bbec5db661ec9803859fb6f89
Batista Harahap <batista@bango29.com>
2020-02-19 02:39:28 +0700

    app yaml env vars

--- src/env.js
+++ src/env.js
@@ -1,7 +1,5 @@
 export default {
   appPort: process.env.PORT || 3000,
   appHost: process.env.HOST || '0.0.0.0',
-  mongoDbConnString:
-    process.env.MONGO_DB_CONN_STRING ||
-    'mongodb+srv://bango:uNrSeRwEbU3Esyk@cuaca-public-wdl9a.gcp.mongodb.net/test?retryWrites=true&w=majority'
+  mongoDbConnString: process.env.MONGO_DB_CONN_STRING || ''
 }
